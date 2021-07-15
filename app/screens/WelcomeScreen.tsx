a20047eaa1a8682f8690afaf2c3b2baacfece573
ElGuayaco <29424456+ElGuayaco@users.noreply.github.com>
2021-07-15 15:11:31 +0200

    CHG update screen

--- app/screens/WelcomeScreen.tsx
+++ app/screens/WelcomeScreen.tsx
@@ -1,16 +1,36 @@
 import React from 'react';
 
-import { StyleSheet, Text } from 'react-native';
+import { ImageBackground, StyleSheet, Text, View } from 'react-native';
 
 function WelcomeScreen() {
     return (
-        <Text>Meal Advisor AI</Text>
+        <View style={styles.container}>
+            <ImageBackground
+                style={styles.background}
+                source={require('../assets/welcome-background.jpg')}>
+
+                <Text style={styles.titleText}>Meal Advisor AI</Text>
+
+            </ImageBackground>
+        </View>
     );
 }
 
 const styles = StyleSheet.create({
-    background: {
-        flex: 1
+    container: {
+        flex:1 , 
+        justifyContent: 'center', 
+        alignItems: 'center'
+    },
+    background: { 
+        width: '110%', 
+        height: '100%', 
+        justifyContent: 'center', 
+        alignItems: 'center' 
+    },
+    titleText: {
+        color: 'black',
+        fontSize: 34
     }
 })
 
