4e92563122093318fd03c952776e29b8e035a998
ElGuayaco <29424456+ElGuayaco@users.noreply.github.com>
2021-07-15 14:39:08 +0200

    ADD welcome screen

---
+++ app/screens/WelcomeScreen.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import { StyleSheet, Text } from 'react-native';
+
+function WelcomeScreen() {
+    return (
+        <Text>Meal Advisor AI</Text>
+    );
+}
+
+const styles = StyleSheet.create({
+    background: {
+        flex: 1
+    }
+})
+
+export default WelcomeScreen;
