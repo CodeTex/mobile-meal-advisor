06ca255064a902f68ba4ab575ab104f2afe96940
ElGuayaco <29424456+ElGuayaco@users.noreply.github.com>
2021-07-16 23:49:36 +0200

    ADD user interaction

---
+++ app/components/userInteraction.tsx
@@ -0,0 +1,56 @@
+import { userMap } from "../data/user.map"
+import { User, UserMap } from "../models/data.model"
+
+
+
+const getInternalName = (user: string, map: UserMap = userMap): string => {
+    return map.get(user) || user;
+}
+
+export const getUser = (user: string): User => {
+
+    user = getInternalName(user)
+
+    let selectedUser: User = {
+        name: user,
+        greeting: 'New in town, eh?',
+        gif: require("../assets/gifs/hello.webp")
+    }
+
+    switch(user.toLowerCase()) {
+        case 'genius':
+            selectedUser['greeting'] = 'A funny one, eh?\nKeeping my photo sensors on you!';
+            selectedUser['gif'] = require("../assets/gifs/suspicious.webp");
+            break;
+        case 'alex':
+            selectedUser['greeting'] = 'It is she who shronketh,\nwelcome!';
+            selectedUser['gif'] = require("../assets/gifs/shronk.webp");
+            break;
+        case 'bernd':
+            selectedUser['greeting'] = 'The most even-tempered buoy,\nahoy!';
+            selectedUser['gif'] = require("../assets/gifs/road_rage.webp");
+            break;
+        case 'christian':
+            selectedUser['greeting'] = 'Dr. Rodenbach I presume,\nmay your sanity persevere!';
+            selectedUser['gif'] = require("../assets/gifs/dentist.webp");
+            break;
+        case 'julian':
+            selectedUser['greeting'] = 'At your service,\nmy liege';
+            selectedUser['gif'] = require("../assets/gifs/bow.webp");
+            break;
+        case 'coco':
+            selectedUser['greeting'] = 'The new cowboy in town,\nbring it on Coco!';
+            selectedUser['gif'] = require("../assets/gifs/cowboy_cat.webp");
+            break;
+        case 'blue':
+            selectedUser['greeting'] = "Duchess Blue,\nI'm at your service!";
+            selectedUser['gif'] = require("../assets/gifs/party_cat.gif");
+            break;
+        case 'emil':
+            selectedUser['greeting'] = 'Ah the feisty Emil,\nen garde!';
+            selectedUser['gif'] = require("../assets/gifs/puss_in_boots.webp");
+            break;
+    }
+
+    return selectedUser
+}
