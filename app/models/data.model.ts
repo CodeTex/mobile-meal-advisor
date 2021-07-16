2fd4712ceed9b5cccf65181a567fb08511389f49
ElGuayaco <29424456+ElGuayaco@users.noreply.github.com>
2021-07-16 21:35:02 +0200

    ADD User, CHG add NodeRequire

--- app/models/data.model.ts
+++ app/models/data.model.ts
@@ -1,9 +1,15 @@
 export type Meal = {
-    image: any,
+    image: NodeRequire,
     title: string, 
     description: string
 };
 
 export type Meals = {[key: string]: Meal};
 
-export type UserMap = Map<string, string>
+export type UserMap = Map<string, string>;
+
+export type User = {
+    name: string,
+    greeting: string,
+    gif: NodeRequire
+}
