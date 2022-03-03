2436b4b184f475a044bf0e18e42e238fcdb482dd
CodeText <29424456+CodeTex@users.noreply.github.com>
2022-03-03 11:57:23 +0100

    CHG minor refactoring

--- flutter/lib/pages/home.dart
+++ flutter/lib/pages/home.dart
@@ -4,7 +4,6 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:mobile_meal_advisor/pages/buffer.dart';
 import 'package:mobile_meal_advisor/pages/result.dart';
-import 'package:mobile_meal_advisor/theme.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -25,55 +24,72 @@ class _HomePageState extends State<HomePage> {
             image: AssetImage("assets/images/login/gray-bowl.jpg"),
           ),
         ),
-        child: BackdropFilter(
-          filter: ImageFilter.blur(sigmaX: 5.0, sigmaY: 5.0),
-          child: Column(
-            children: <Widget>[
-              const Expanded(
-                child: Center(
-                  child: Text("Text"),
-                ),
+        child: Column(
+          children: const <Widget>[
+            Expanded(
+              child: Center(
+                child: Text("Text"),
               ),
-              Expanded(
-                child: ElevatedButton(
-                  child: Text(
-                    "Choose my meal",
-                    style: GoogleFonts.roboto(
-                      color: Colors.black54,
-                      fontSize: 24,
-                      fontWeight: FontWeight.bold,
-                    ),
-                    textAlign: TextAlign.center,
-                  ),
-                  onPressed: () async {
-                    await Future.delayed(const Duration(milliseconds: 400), () {});
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (_) => const BufferPage(),
-                      ),
-                    );
-                    await Future.delayed(const Duration(seconds: 12), () {});
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (_) => const ResultPage(),
-                      ),
-                    );
-                  },
-                  style: ElevatedButton.styleFrom(
-                    fixedSize: const Size(200, 200),
-                    shape: const CircleBorder(
-                        side: BorderSide(
-                      color: Colors.black54,
-                      width: 4,
-                    )),
-                  ),
-                ),
-              ),
-            ],
-          ),
+            ),
+            Expanded(
+              child: HomePageButton(),
+            ),
+          ],
         ),
+      ),
+    );
+  }
+}
+
+class HomePageButton extends StatelessWidget {
+  const HomePageButton({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      child: Text(
+        "GO",
+        style: GoogleFonts.roboto(
+          color: Colors.black54,
+          fontSize: 48,
+          fontWeight: FontWeight.bold,
+        ),
+        textAlign: TextAlign.center,
+      ),
+      onLongPress: () {
+        _navigateToResults(context);
+      },
+      onPressed: () async {
+        await Future.delayed(const Duration(milliseconds: 400), () {});
+        _navigateToBuffer(context);
+        await Future.delayed(const Duration(seconds: 12), () {});
+        _navigateToResults(context);
+      },
+      style: ElevatedButton.styleFrom(
+        fixedSize: const Size(200, 200),
+        shape: const CircleBorder(
+            side: BorderSide(
+          color: Colors.black54,
+          width: 6,
+        )),
+      ),
+    );
+  }
+
+  void _navigateToBuffer(BuildContext context) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (_) => const BufferPage(),
+      ),
+    );
+  }
+
+  void _navigateToResults(BuildContext context) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (_) => const ResultPage(),
       ),
     );
   }
