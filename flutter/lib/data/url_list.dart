67fa47de60f3bfa83989ac2b750356f17cd7c1df
CodeText <29424456+CodeTex@users.noreply.github.com>
2022-03-17 17:41:00 +0100

    CHG update url list

--- flutter/lib/data/url_list.dart
+++ flutter/lib/data/url_list.dart
@@ -1,10 +1,11 @@
 List<String> randomUrlList = <String>[
-  "http://ninjaflex.com/",
+  // "http://ninjaflex.com/",
+  "https://pointerpointer.com/",
   "http://endless.horse/",
   "https://thatsthefinger.com/",
-  "http://burymewithmymoney.com/",
-  "https://quickdraw.withgoogle.com/",
-  "http://crossdivisions.com/",
+  // "http://burymewithmymoney.com/",
+  // "https://quickdraw.withgoogle.com/",
+  // "http://crossdivisions.com/",
   "https://cat-bounce.com/",
-  "http://www.koalastothemax.com/",
+  // "http://www.koalastothemax.com/",
 ];
