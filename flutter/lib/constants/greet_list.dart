97a8c0ed4dc40f76322c6173cd77b868b49ddd83
CodeTex <29424456+CodeTex@users.noreply.github.com>
2022-04-27 16:46:58 +0200

    ADD greeting randomly

---
+++ flutter/lib/constants/greet_list.dart
@@ -0,0 +1,12 @@
+// greetings with an exclamation mark
+const List<String> greetingsEm = <String>[
+  "hello",
+  "greetings",
+  "howdi",
+  "hello there",
+  "welcome",
+  "hola"
+];
+
+// greetings with a question mark
+const List<String> greetingsQm = <String>["wazzup", "sup", "how's it hanging"];
--- flutter/lib/widgets/home/menu.dart
+++ flutter/lib/widgets/home/menu.dart
@@ -1,5 +1,9 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:mobile_meal_advisor/constants/greet_list.dart';
+import 'package:mobile_meal_advisor/functions/string.dart';
 import 'package:mobile_meal_advisor/theme.dart';
 
 class HomePageMenu extends StatelessWidget {
@@ -16,8 +20,33 @@ class HomePageMenu extends StatelessWidget {
 
   String greet(String? userName) {
     String greeting = "Hello";
+    String lastCharacter = "";
+    List<String> greetings = [];
+    int hourOfDay = DateTime.now().hour;
+
+    greetings.addAll(greetingsEm);
+    greetings.addAll(greetingsQm);
+    if (hourOfDay < 11) {
+      greetings.add("good morning");
+    } else if (hourOfDay < 14) {
+      greetings.add("mahlzeit");
+    } else if (hourOfDay < 18) {
+      greetings.add("good afternoon");
+    } else {
+      greetings.add("good evening");
+    }
+
+    if (greetings.isNotEmpty) {
+      int index = Random().nextInt(greetings.length);
+      greeting = capitalize(greetings[index]);
+      if (greetingsEm.contains(greetings[index])) {
+        lastCharacter = "!";
+      } else if (greetingsQm.contains(greetings[index])) {
+        lastCharacter = "?";
+      }
+    }
     if (userName != null) greeting += ", " + userName;
-    return greeting + "!";
+    return greeting + lastCharacter;
   }
 
   @override
