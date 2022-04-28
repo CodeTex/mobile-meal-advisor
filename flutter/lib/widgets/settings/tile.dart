4f4dc4b7d5d83f8ac6ab33903d9683f4cf0630a4
CodeTex <29424456+CodeTex@users.noreply.github.com>
2022-04-28 20:41:24 +0200

    CHG updated padding

--- flutter/lib/widgets/settings/tile.dart
+++ flutter/lib/widgets/settings/tile.dart
@@ -18,33 +18,33 @@ class SettingsTile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 120,
       padding: const EdgeInsets.all(10),
       margin: const EdgeInsets.only(bottom: 10),
       color: Palette.primary.withOpacity(.4),
-      child: Column(
-        children: <Widget>[
-          Align(
-            alignment: Alignment.centerLeft,
-            child: Text(
-              _withColon(title),
-              style: const TextStyle(
-                fontSize: 14,
-                fontWeight: FontWeight.bold,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 5),
+        child: Column(
+          children: <Widget>[
+            Align(
+              alignment: Alignment.centerLeft,
+              child: Text(
+                _withColon(title),
+                style: const TextStyle(
+                  fontSize: 14,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
             ),
-          ),
-          const Padding(
-            padding: EdgeInsets.symmetric(vertical: 5),
-            child: Divider(
-              height: 2,
-              indent: 5,
-              endIndent: 5,
-              color: Palette.border,
+            const Padding(
+              padding: EdgeInsets.symmetric(vertical: 5),
+              child: Divider(
+                height: 2,
+                color: Palette.border,
+              ),
             ),
-          ),
-          widget
-        ],
+            widget
+          ],
+        ),
       ),
     );
   }
