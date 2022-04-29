a764d44c27783c6dfd7822e4b80dba3d5465a7c7
CodeTex <29424456+CodeTex@users.noreply.github.com>
2022-04-29 19:52:42 +0200

    ADD store values and types

--- flutter/lib/models/store.dart
+++ flutter/lib/models/store.dart
@@ -1,2 +1,8 @@
 typedef StoreUserName = String;
+String keyUserName = "userName";
+
 typedef StoreLogState = bool;
+String keyLogState = "logState";
+
+typedef StoreFilterCategories = List<String>;
+String keyFilterCategories = "filterCategories";
--- flutter/lib/widgets/settings/filter_category.dart
+++ flutter/lib/widgets/settings/filter_category.dart
@@ -1,12 +1,37 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:mobile_meal_advisor/constants/meal_map.dart';
 import 'package:mobile_meal_advisor/functions/string.dart';
+import 'package:mobile_meal_advisor/models/store.dart';
 import 'package:mobile_meal_advisor/widgets/settings/tile.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-class SettingsFilterCategory extends StatelessWidget {
+class SettingsFilterCategory extends StatefulWidget {
+  const SettingsFilterCategory({Key? key}) : super(key: key);
+
+  @override
+  State<SettingsFilterCategory> createState() => _SettingsFilterCategoryState();
+}
+
+class _SettingsFilterCategoryState extends State<SettingsFilterCategory> {
   final List<String> mealCategories = mealCategoryMap.values.toList();
 
-  SettingsFilterCategory({Key? key}) : super(key: key);
+  @override
+  void initState() {
+    super.initState();
+    sharedPreferencesInit();
+  }
+
+  void sharedPreferencesInit() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    StoreFilterCategories? activeFilters = prefs.getStringList(keyFilterCategories);
+    if (activeFilters == null) {
+      log("Store for key " + keyFilterCategories + " return 'null'.");
+      return;
+    }
+    setState(() {});
+  }
 
   @override
   Widget build(BuildContext context) {
