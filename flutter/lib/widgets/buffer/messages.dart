5666aba1e686917dc55db13d3483590477b32d35
CodeTex <29424456+CodeTex@users.noreply.github.com>
2022-04-30 11:41:52 +0200

    ADD periodic message update

--- flutter/lib/widgets/buffer/messages.dart
+++ flutter/lib/widgets/buffer/messages.dart
@@ -1,4 +1,8 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:mobile_meal_advisor/constants/progress_messages.dart';
+import 'package:mobile_meal_advisor/functions/math.dart';
 import 'package:mobile_meal_advisor/widgets/glass_morphism.dart';
 
 class BufferMessages extends StatefulWidget {
@@ -9,15 +13,35 @@ class BufferMessages extends StatefulWidget {
 }
 
 class _BufferMessagesState extends State<BufferMessages> {
+  String _message = "";
+
+  @override
+  void initState() {
+    super.initState();
+    setState(() {
+      _message = _updateMessage();
+    });
+    Timer.periodic(const Duration(seconds: 4), (Timer timer) {
+      setState(() {
+        _message = _updateMessage();
+      });
+    });
+  }
+
+  String _updateMessage() {
+    int index = randint(0, progressMessages.length - 1);
+    return progressMessages[index];
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Center(
+    return Center(
       child: GlassContainer(
         child: SizedBox(
           width: double.infinity,
           child: Padding(
-            padding: EdgeInsets.all(10),
-            child: Center(child: Text("Title")),
+            padding: const EdgeInsets.all(10),
+            child: Center(child: Text(_message)),
           ),
         ),
       ),
